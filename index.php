2b83b955647724c9ccd7143c1d825982fccfee25
Worshox <worshox.28@gmail.com>
2022-07-08 22:47:29 +0200

    Add logging system and basic error styling

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+
+// if (isset($_SESSION['username']))
+//     header('Location: home');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -29,4 +36,43 @@
 </html>
 
 <?php
-#TODO - ADD LOGIN SYSTEM
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $form_login = $_POST['login'];
+    $form_password = $_POST['password'];
+
+    if (empty($form_login) || empty($form_password)) {
+        echo "<p id='error'>Both fields have to be filled</p>";
+        session_destroy();
+    } else {
+
+        $pdo = new PDO("mysql:host=localhost;dbname=roseland", "root", "");
+        $sql = "SELECT * FROM `login_system` WHERE `user_login` = :form_login";
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindValue(':form_login', $form_login);
+        $stmt->execute();
+
+        $db_login = $stmt->fetch(PDO::FETCH_NUM);
+
+        if (empty($db_login))
+            echo "<p id='error'>Wrong login</p>";
+        else {
+            $sql = "SELECT user_password FROM `login_system` WHERE `user_login` = :form_login";
+            $stmt = $pdo->prepare($sql);
+
+            $stmt->bindValue(':form_login', $form_login);
+            $stmt->execute();
+
+            $db_password = $stmt->fetch(PDO::FETCH_NUM);
+
+            if (strcmp($db_password[0], $form_password))
+                echo "<p id='error'>Wrong password</p>";
+            else {
+                $_SESSION['username'] = $db_login;
+            }
+        }
+    }
+}
+
+?>
